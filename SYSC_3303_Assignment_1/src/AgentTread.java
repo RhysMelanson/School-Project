f440315539991514a7e4e2a221a41a1bb0f8abf9
RhysMelanson <rhys.melanson@yahoo.com>
2020-01-20 17:30:11 -0500

    Add files via upload

---
+++ SYSC_3303_Assignment_1/src/AgentTread.java
@@ -0,0 +1,97 @@
+import java.util.Random;
+/**
+ * 
+ * @author Rhys
+ *
+ */
+public class AgentTread implements Runnable {
+
+	public static String neededIngredient; //The ingredient required from a chef
+	private int sandwichQuota = 20; //Number of sandwiches chefs must produce
+	public volatile int NumSandwiches = 0; //Counter to track sandwiches produced
+	private static AgentTread cs = new AgentTread();
+	private static Thread thread = new Thread(cs, "New Thread");
+
+	/**
+	 * Starts the agent thread.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		thread.start();
+	}
+
+	/**
+	 * Initializes chef threads then continuously chooses one ingredient that is required 
+	 * to make a sandwich. The thread then notifies chefs that an ingredient is required
+	 * and wait till notified by the active chef to repeat the process until 20 sandwiches
+	 * are made.
+	 */
+	@Override
+	public void run() {
+		
+		//Initialize all the chef thread
+		ChefThread BreadChef = new ChefThread("Bread", cs, sandwichQuota);
+		ChefThread JamChef = new ChefThread("Jam", cs, sandwichQuota);
+		ChefThread PenutButterChef = new ChefThread("Penut Butter", cs, sandwichQuota);
+		//Start all the chef threads
+		BreadChef.start();
+		JamChef.start();
+		PenutButterChef.start();
+		
+		//Holder variable used to track the thread to synchronize with agent 
+		//based on which chef has the needed ingredient
+		ChefThread temp;
+		
+		while (true) {
+
+			//Random value to select ingredients at random
+			int rand = (new Random()).nextInt((3 - 1) + 1) + 1;
+
+			//Using the random variable to put the two ingredients chosen and decide the
+			//the needed ingredient
+			if (rand == 1) {
+				neededIngredient = "Jam";
+				System.out.println("Agent released Bread and Penut Butter");
+				temp = JamChef;
+
+			} else if (rand == 2) {
+				neededIngredient = "Bread";
+				System.out.println("Agent released Jam and Penut Butter");
+				temp = BreadChef;
+
+			} else {
+				neededIngredient = "Penut Butter";
+				System.out.println("Agent released Bread and Jam");
+				temp = PenutButterChef;
+			}
+			
+			//notifies all threads waiting in make sandwich to wake up
+			//and check loop condition
+			synchronized (cs) {
+				notifyAll();
+			}
+			
+			//Wait on the executing chef to finish its sandwich
+			synchronized (temp) {
+				try {
+					temp.wait();
+				} catch (InterruptedException e) {}
+			}
+			
+			//Increase sandwich counter
+			NumSandwiches++;
+			
+			//If the chefs have met the sandwich quota notify waiting threads then terminate
+			if(NumSandwiches == sandwichQuota) {
+				System.out.println("The chefs have made " + sandwichQuota + " sandwiches.");
+				synchronized (cs) {
+					notifyAll();
+				}
+				break;
+			}
+		}
+		System.out.println("Agent thread has terminated");
+	}
+}
---
+++ SYSC_3303_Assignment_1/src/ChefThread.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ * @author Rhys
+ *
+ */
+public class ChefThread extends Thread {
+	
+	private String ingredient; //This chef's ingredient
+	private int sandwichQuota; //Number of sandwiches chefs must produce
+	private AgentTread ag;
+	
+	/**
+	 * Constructor for a new chef thread contains agent which initialized
+	 * ingredient possessed by the chef, and total amount of sandwiches to be produced.
+	 * 
+	 * @param type
+	 * @param agent
+	 */
+	public ChefThread(String type, AgentTread agent, int quota) {
+		sandwichQuota = quota;
+		ag = agent;
+		ingredient = type;
+	}
+
+	/**
+	 * Tries to make sandwiches, 
+	 * eating then notifying the agent after one is made,
+	 * this process is repeated until sandwich quota is met.
+	 */
+	@Override
+	public void run() {
+
+		//each chef will continuously make sandwiches until 20 are made
+		while (ag.NumSandwiches < sandwichQuota) {                            
+			makeSandwich(ingredient);
+			
+			//If the sandwich quota is met stop making sandwiches
+			//and break without trying to eat
+			if(ag.NumSandwiches == sandwichQuota) {
+				break;
+			}
+			
+			//When the sandwich is made notify the Agent to release new ingredients
+			synchronized (this) {
+				notifyAll();
+			}
+			
+			//Sleep to represent the time it take for the chef to eat the sandwich
+			System.out.println(ingredient + " Chef is eating the sandwich");
+			
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {}
+		}
+		System.out.println(ingredient + " Chef thread has terminated");
+	}
+
+	/**
+	 * Waits till the possessed ingredient is the one required 
+	 * and when condition is met make a sandwich. Contains condition to free threads
+	 * once sandwich quota is met.
+	 * 
+	 * @param ingredient
+	 */
+	public void makeSandwich(String ingredient) {
+
+		//Loops until the chef's ingredient is the one needed
+		//or the required amount of sandwiches are made
+		while (ingredient != AgentTread.neededIngredient && ag.NumSandwiches != sandwichQuota) {
+			
+			//Synchronized with agent thread
+			synchronized (ag) {
+				
+				//Thread waits till notified by agent
+				try {
+					ag.wait();
+				} catch (InterruptedException e) {}
+			}
+		}
+
+		//Break if the required amount of sandwiches are produced
+		if(ag.NumSandwiches == sandwichQuota) {
+			return;
+		}
+		
+		//Sleep to represent the time it take for the chef to make the sandwich
+		System.out.println(ingredient + " Chef is making a sandwich.");
+
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {}
+	}
+}
